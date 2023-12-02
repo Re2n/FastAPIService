b8075c4e85f95ea4c91e47c892ea16b73b8d56f8
Re2n <111135217+Re2n@users.noreply.github.com>
2023-12-02 21:48:14 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from routers import users
+from schemas.db import database
+
+app = FastAPI(
+    title="Trading App"
+)
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+@app.on_event('shutdown')
+async def startup():
+    await database.disconnect()
+
+app.include_router(users.router)
