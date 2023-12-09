d4d3bd54a987223abdd9cec3a836ccf22aca8207
Re2n <111135217+Re2n@users.noreply.github.com>
2023-12-09 20:12:42 +0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -6,3 +9,40 @@ class ModelUser(BaseModel):
     password: str
     is_admin: bool
     balance: float
+
+
+class ModelTransport(BaseModel):
+    ownerusername: str
+    canBeRented: bool
+    transportType: str
+    model: str
+    color: str
+    identifier: str
+    description: Optional[str]
+    latitude: float
+    longitude: float
+    minutePrice: Optional[float]
+    dayPrice: Optional[float]
+
+
+class UserModelTransport(BaseModel):
+    canBeRented: bool
+    transportType: str
+    model: str
+    color: str
+    identifier: str
+    description: Optional[str]
+    latitude: float
+    longitude: float
+    minutePrice: Optional[float]
+    dayPrice: Optional[float]
+
+
+class RentModel(BaseModel):
+    transportidentifier: str
+    ownerusername: str
+    timeStart: str
+    timeEnd: Optional[str]
+    priceOfUnit: float
+    priceType: str
+    finalPrice: Optional[float]
