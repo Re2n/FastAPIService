6842f868c06eabb84f0f590881cf9ff2b14423e7
Re2n <111135217+Re2n@users.noreply.github.com>
2023-12-09 23:07:45 +0300

    Update db.py

--- schemas/db.py
+++ schemas/db.py
@@ -1,5 +1,5 @@
 import databases
 
-sql_url = f'postgresql://admin:admin@localhost:5431/production'
+sql_url = f'postgresql://admin:admin@database:5432/production'
 
 database = databases.Database(sql_url)
